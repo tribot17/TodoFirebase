e045568c6c6052502931c0cb5cd1c341cf65ca9b
Tristan <tribot17@gmail.com>
2022-06-06 16:42:46 +0200

    update front

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <ul className="list_social">
+        <li>
+          <a href="https://www.linkedin.com/in/tristan-boettger-magnier-862378210/" target="_blank" rel="noreferrer">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://github.com/tribot17" target="_blank" rel="noreferrer">GitHub</a>
+        </li>
+        <li>
+          <a href="https://www.malt.fr/profile/tristanboettger" target="_blank" rel="noreferrer">Malt</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,23 +1,31 @@
 import React, { useRef, useState } from "react";
-
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { RiCloseCircleLine } from "react-icons/ri";
-require("dotenv").config({ path: "../.env" });
+import Footer from "./Footer";
+require("dotenv").config();
 
-console.log(process.env);
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAING_SENDER_ID,
+  REACT_APP_APP_ID,
+  REACT_APP_MEASURMENT_ID,
+} = process.env;
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASURMENT_ID,
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAING_SENDER_ID,
+  appId: REACT_APP_APP_ID,
+  measurementId: REACT_APP_MEASURMENT_ID,
 });
 
 const auth = firebase.auth();
@@ -62,7 +70,6 @@ function Todos() {
   const [todo] = useCollectionData(query, { idField: "id" });
   const user = firebase.auth().currentUser;
   const { uid } = auth.currentUser;
-  
 
   const addTodo = async (long) => {
     await todoRef.add({
@@ -114,6 +121,7 @@ function Todos() {
 
   //----------------------------------Delete
   const deleteTodo = async (tod) => {
+    console.log(tod);
     todoRef.doc(tod.id).delete();
   };
 
@@ -127,7 +135,7 @@ function Todos() {
   const displayTodo = (tod) => {
     if (tod.createdAt === null) {
       return (
-        <div class="todoCard">
+        <div className="todoCard">
           <li draggable="true" data-draggable="item">
             <h4> {tod.text} </h4>
             <p> crée le {Date.now()} </p>
@@ -140,7 +148,7 @@ function Todos() {
       );
     } else {
       return (
-        <div class="todoCard">
+        <div className="todoCard">
           <li draggable="true" data-draggable="item">
             <h4> {tod.text} </h4>
             <p> crée le {datePaser(tod.createdAt.seconds)} </p>
@@ -156,7 +164,7 @@ function Todos() {
   //----------------------------------Display
   return (
     <>
-      <div class="bar">
+      <div className="bar">
         <h1>ToDo List de {user.displayName}</h1>
         <SignOut />
       </div>
@@ -165,17 +173,18 @@ function Todos() {
       <div className="enterTodo">
         <form onSubmit={sendTodo}>
           <input
+            id="TodoValue"
             value={formValue}
             onChange={(e) => setFormValue(e.target.value)}
           />
           {errorText && <p>Veuillez entrer un todo</p>}
           <div id="checkbox">
             <input type="checkbox" name="courtTerme" id="courtTerme"></input>
-            <label for="courtTerme">Court terme</label>
+            <label htmlFor="courtTerme">Court terme</label>
             <input type="checkbox" name="moyenTerme" id="moyenTerme"></input>
-            <label for="moyenTerme">Moyen terme</label>
+            <label htmlFor="moyenTerme">Moyen terme</label>
             <input type="checkbox" name="longTerme" id="longTerme"></input>
-            <label for="longTerme">Long terme</label>
+            <label htmlFor="longTerme">Long terme</label>
             {errorBox && <p>Veuillez cocher une case</p>}
             {errorBoxs && <p>Veuillez cocher une seule case</p>}
           </div>
@@ -189,49 +198,32 @@ function Todos() {
         <div className="courtTerme">
           <h2>Court terme :</h2>
           {todo &&
-            todo.map((tod) => {
-              return tod.uid === uid ? (
-                tod.longeur === "court" ? (
-                  displayTodo(tod)
-                ) : (
-                  <div></div>
-                )
-              ) : (
-                null
-              );
-            })}
+            todo.map(
+              (tod, i) =>
+                tod.uid === uid &&
+                tod.longeur === "court" && <div key={i}>{displayTodo(tod)}</div>
+            )}
         </div>
         <div className="moyenTerme">
           <h2>Moyen terme :</h2>
           {todo &&
-            todo.map((tod) => {
-              return tod.uid === uid ? (
-                tod.longeur === "moyen" ? (
-                  displayTodo(tod)
-                ) : (
-                  null
-                )
-              ) : (
-                null
-              );
-            })}
+            todo.map(
+              (tod, i) =>
+                tod.uid === uid &&
+                tod.longeur === "moyen" && <>{displayTodo(tod)}</>
+            )}
         </div>
         <div className="longTerme">
           <h2>Long terme :</h2>
           {todo &&
-            todo.map((tod) => {
-              return tod.uid === uid ? (
-                tod.longeur === "long" ? (
-                  displayTodo(tod)
-                ) : (
-                  null
-                )
-              ) : (
-                null
-              );
-            })}
+            todo.map(
+              (tod, i) =>
+                tod.uid === uid &&
+                tod.longeur === "long" && <div key={i}>{displayTodo(tod)}</div>
+            )}
         </div>
       </div>
+      <Footer/>
     </>
   );
 }
